153431296ca790c173f9201552b0300103d93593
b06501001 <58772368+b06501001@users.noreply.github.com>
2019-12-23 19:55:54 +0800

    Update Button.py

--- Button.py
+++ Button.py
@@ -5,13 +5,13 @@ import functools
 # 設定視窗
 window = tk.Tk()
 window.title('Time Selection')
-window.geometry('800x600')
+window.geometry('800x800')
 window.configure(background='white')
 
 top_label = tk.Label(window, text='Choose the time occupied !', bg='white', font=('System', 12))
 top_label.pack()
 
-hit = [[False for i in range(10)] for j in range(5)]
+hit = [[False for i in range(14)] for j in range(5)]
 # print(hit)
 
 
@@ -34,18 +34,18 @@ frame_1.pack(side='left')
 frame_2.pack(side='right')
 
 # 建立button
-time_button = [[None for i in range(10)] for j in range(5)]
-time_result = [[None for i in range(10)] for j in range(5)]
+time_button = [[None for i in range(14)] for j in range(5)]
+time_result = [[None for i in range(14)] for j in range(5)]
 
 for i in range(5):
-    for j in range(10):
+    for j in range(14):
         time_button[i][j] = tk.Button(frame_1, text='', bg='green', font=('System', 12),
                                       width=3, height=1, command=functools.partial(time_hit, i, j))
         time_button[i][j].grid(row=j, column=i, padx=5, pady=5, ipadx=5, ipady=5)
 
 # 建立選擇後的狀態
 for i in range(5):
-    for j in range(10):
+    for j in range(14):
         time_result[i][j] = tk.Label(frame_2, text='O', bg='green', font=('System', 12), width=4, height=1)
         time_result[i][j].grid(row=j, column=i, padx=5, pady=5, ipadx=5, ipady=5)
 
@@ -54,7 +54,7 @@ window.mainloop()
 
 time_available = hit
 for i in range(5):
-    for j in range(10):
+    for j in range(14):
         if time_available[i][j]:
             time_available[i][j] = 0
         else:
