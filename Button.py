4f20476cdac77527c2dd517d9c5c6e9c1b319165
pearli319 <b04102108@ntu.edu.tw>
2019-12-28 11:40:59 +0800

    url

--- Button.py
+++ Button.py
@@ -170,8 +170,8 @@ from selenium.webdriver.common.keys import Keys
 
 
 columns_1f = []
-driver = webdriver.Chrome(r"C:\Users\User\Desktop\chromedriver")
-# driver = webdriver.Chrome(r"/Users/zizhenli/Downloads/chromedriver")
+# driver = webdriver.Chrome(r"C:\Users\User\Desktop\chromedriver")
+driver = webdriver.Chrome(r"/Users/zizhenli/Downloads/chromedriver")
 
 # In[59]:
 
